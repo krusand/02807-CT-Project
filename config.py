db97c16569cc7b2e89a3c2e7e857130c7e286dcd
krusand <110490348+krusand@users.noreply.github.com>
2025-11-12 12:52:19 +0100

    feat: added data paths for each file

--- config.py
+++ config.py
@@ -8,5 +8,11 @@ DATA_RAW_DIR = DATA_DIR / "raw"
 DATA_CLEANED_DIR = DATA_DIR / "cleaned"
 DATA_PREPROCESSED_DIR = DATA_DIR / "preprocessed"
 
+AISLES_PATH = DATA_CLEANED_DIR / "aisles.pq"
+DEPARTMENTS_PATH = DATA_CLEANED_DIR / "departments.pq"
+ORDER_PRODUCTS__PRIOR_PATH = DATA_CLEANED_DIR / "order_products__prior.pq"
+ORDER_PRODUCTS__TRAIN_PATH = DATA_CLEANED_DIR / "order_products__train.pq"
+ORDERS_PATH = DATA_CLEANED_DIR / "orders.pq"
+PRODUCTS_PATH = DATA_CLEANED_DIR / "products.pq"
 
 
