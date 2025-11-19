c9146178daf181538843e93bfbe647f3aa0dc4e0
Andreas Kruse Svenningsen <krusand@live.dk>
2025-11-19 10:06:01 +0100

    chore: bash script

--- bash_script.sh
+++ bash_script.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-### ------------- specify job name ----------------
 #BSUB -J test_ct_aks 
-### ------------- specify number of cores ----------------
 #BSUB -n 1
 #BSUB -R "span[hosts=1]"
 
-#BSUB -R "rusage[mem=20GB]"
+#BSUB -R "rusage[mem=30GB]"
 
 ### ------------- specify wall-clock time (max allowed is 12:00)---------------- 
 #BSUB -W 01:00
 
-#BSUB -o outputs/%J/script.out
-#BSUB -e outputs/%J/script.err
+#BSUB -o outputs/%J.out
+#BSUB -e outputs/%J.err
 
 source .venv/bin/activate
 python pipeline.py
