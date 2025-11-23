bf6c0fb1abc52e075570bdea5084870e2d013a8e
Andreas Kruse Svenningsen <krusand@live.dk>
2025-11-23 19:24:48 +0100

    added logging

--- bash_script.sh
+++ bash_script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#BSUB -J test_ct_aks 
+#BSUB -J __256gb_memory
 #BSUB -n 1
 #BSUB -R "span[hosts=1]"
 #BSUB -q milan
 
-#BSUB -R "rusage[mem=50GB]"
+#BSUB -R "rusage[mem=256GB]"
 
 ### ------------- specify wall-clock time (max allowed is 12:00)---------------- 
 #BSUB -W 01:00
@@ -13,4 +13,5 @@
 #BSUB -e outputs/%J.err
 
 source .venv/bin/activate
-python pipeline.py
+#python pipeline.py
+python scripts/collaborative_filter.py
