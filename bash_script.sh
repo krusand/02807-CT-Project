a3880da1e2e80e1c9cc61d256f8441e5d1483091
SebWae <s254120@dtu.dk>
2025-11-25 20:32:53 +0100

    test: performing cf experiments with aisles

--- bash_script.sh
+++ bash_script.sh
@@ -14,7 +14,7 @@
 
 source .venv/bin/activate
 #python pipeline.py
-python collaborative_filter_experiments.py \
+python experiments/cf_all_users_aisles_experiments.py \
   --n_features "500,750,1000,1250,1500" \
   --reg_vals "0.001,0.01,0.1" \
   --damping_vals "5,10" \
