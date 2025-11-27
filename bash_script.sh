90c8cda71a89a32b8919ea7594f382d0d1f8a47b
SebWae <s254120@dtu.dk>
2025-11-27 21:51:58 +0100

    feat: hyperparam tuning for mba recommendations

--- bash_script.sh
+++ bash_script.sh
@@ -4,18 +4,16 @@
 #BSUB -R "span[hosts=1]"
 #BSUB -q milan
 
-#BSUB -R "rusage[mem=256GB]"
+#BSUB -R "rusage[mem=64GB]"
 
 ### ------------- specify wall-clock time (max allowed is 12:00)---------------- 
-#BSUB -W 06:00
+#BSUB -W 12:00
 
 #BSUB -o outputs/%J.out
 #BSUB -e outputs/%J.err
 
 source .venv/bin/activate
 #python pipeline.py
-python experiments/cf_all_users_aisles_experiments.py \
-  --n_features "500,750,1000,1250,1500" \
-  --reg_vals "0.001,0.01,0.1" \
-  --damping_vals "5,10" \
-  --bias 1
+python experiments/mba_experiments.py \
+  --support_grids "0.00001,0.0001" \
+  --min_confidences "0.001,0.005,0.01,0.05,0.1 \
