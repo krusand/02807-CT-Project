767827bef708a7afeb0be7d04454f0a0f1a08369
SebWae <s254120@dtu.dk>
2025-12-04 09:46:48 +0100

    chore: uncommenting all cf recommenders

--- scripts/collaborative_filter.py
+++ scripts/collaborative_filter.py
@@ -40,75 +40,75 @@ def main():
 
 
     ### CF - ALL USERS ALL ITEMS ###
-    # logging.info("Starting CF fit - all users and all items")
-    # pred_ratings, mf = get_cf_scores(features=2000, 
-    #                                  rating_df_train=train_ratings, 
-    #                                  rating_df_val=val_ratings, 
-    #                                  reg=0.001,
-    #                                  bias=False)
-    # logging.info("Ended CF fit - all users and all items")
+    logging.info("Starting CF fit - all users and all items")
+    pred_ratings, mf = get_cf_scores(features=2000, 
+                                     rating_df_train=train_ratings, 
+                                     rating_df_val=val_ratings, 
+                                     reg=0.001,
+                                     bias=False)
+    logging.info("Ended CF fit - all users and all items")
 
-    # logging.info("Starting recs")
-    # recs_dict = get_recs(pred_ratings=pred_ratings, mf=mf, n_recs=n_recs)
-    # logging.info("Ending recs")
-    # with open(DATA_PREPROCESSED_DIR / "cf_all_users_all_items_recs.pkl", 'wb') as fp:
-    #     pkl.dump(recs_dict,file=fp)
+    logging.info("Starting recs")
+    recs_dict = get_recs(pred_ratings=pred_ratings, mf=mf, n_recs=n_recs)
+    logging.info("Ending recs")
+    with open(DATA_PREPROCESSED_DIR / "cf_all_users_all_items_recs.pkl", 'wb') as fp:
+        pkl.dump(recs_dict,file=fp)
 
-    # logging.info("Saved recs")
+    logging.info("Saved recs")
 
-    # logging.info("Evaluating performance of CF recommender (all items, all users)")
-    # test_products = construct_test_product_dict(mode="test")
-    # eval_dict = eval_recs(recs_dict=recs_dict, rating_df=test_ratings, test_products=test_products)
+    logging.info("Evaluating performance of CF recommender (all items, all users)")
+    test_products = construct_test_product_dict(mode="test")
+    eval_dict = eval_recs(recs_dict=recs_dict, rating_df=test_ratings, test_products=test_products)
     
-    # # computing average hit-rate and ndcg
-    # avg_hr = np.mean([metric_dict["hit-rate"] for metric_dict in eval_dict.values()])
-    # avg_ndcg = np.mean([metric_dict[f"ndcg@{n_recs}"] for metric_dict in eval_dict.values()])
+    # computing average hit-rate and ndcg
+    avg_hr = np.mean([metric_dict["hit-rate"] for metric_dict in eval_dict.values()])
+    avg_ndcg = np.mean([metric_dict[f"ndcg@{n_recs}"] for metric_dict in eval_dict.values()])
 
-    # logging.info(f"The average hit-rate on the test set across all users is {avg_hr:.6f}")
-    # logging.info(f"The average ndcg@{n_recs} on the test set across all users is {avg_ndcg:.6f}")
+    logging.info(f"The average hit-rate on the test set across all users is {avg_hr:.6f}")
+    logging.info(f"The average ndcg@{n_recs} on the test set across all users is {avg_ndcg:.6f}")
 
-    # row = ["cf_all_users_all_items", n_recs, avg_hr, avg_ndcg]
-    # with open(RESULTS_PATH, mode="a", newline="") as file:
-    #     writer = csv.writer(file)
-    #     writer.writerow(row)
+    row = ["cf_all_users_all_items", n_recs, avg_hr, avg_ndcg]
+    with open(RESULTS_PATH, mode="a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
 
 
     # # ### CF - ALL USERS AISLES ###
-    # logging.info("Starting CF fit - all users aisles")
-    # pred_ratings, mf = get_cf_scores(features=500, 
-    #                                  rating_df_train=train_ratings_aisles, 
-    #                                  rating_df_val=val_ratings, 
-    #                                  reg=0.0001,
-    #                                  damping=5,
-    #                                  bias=True,
-    #                                  aisles=True,
-    #                                  aisle_dict=aisle_dict)
-    # logging.info("Ended CF fit - all users aisles")
+    logging.info("Starting CF fit - all users aisles")
+    pred_ratings, mf = get_cf_scores(features=500, 
+                                     rating_df_train=train_ratings_aisles, 
+                                     rating_df_val=val_ratings, 
+                                     reg=0.0001,
+                                     damping=5,
+                                     bias=True,
+                                     aisles=True,
+                                     aisle_dict=aisle_dict)
+    logging.info("Ended CF fit - all users aisles")
 
-    # logging.info("Starting recs")
-    # aisle_recs_dict = get_recs(pred_ratings=pred_ratings, mf=mf, n_recs=n_recs)
-    # recs_dict = convert_aisle_recs(recs_dict=aisle_recs_dict, aisle_top_products=aisle_dict)
-    # logging.info("Ending recs")
-    # with open(DATA_PREPROCESSED_DIR / "cf_all_users_aisles_recs.pkl", 'wb') as fp:
-    #     pkl.dump(recs_dict,file=fp)
+    logging.info("Starting recs")
+    aisle_recs_dict = get_recs(pred_ratings=pred_ratings, mf=mf, n_recs=n_recs)
+    recs_dict = convert_aisle_recs(recs_dict=aisle_recs_dict, aisle_top_products=aisle_dict)
+    logging.info("Ending recs")
+    with open(DATA_PREPROCESSED_DIR / "cf_all_users_aisles_recs.pkl", 'wb') as fp:
+        pkl.dump(recs_dict,file=fp)
 
-    # logging.info("Saved recs")
+    logging.info("Saved recs")
 
-    # logging.info("Evaluating performance of CF recommender (all users, aisles)")
-    # test_products = construct_test_product_dict(mode="test")
-    # eval_dict = eval_recs(recs_dict=recs_dict, rating_df=test_ratings, test_products=test_products)
+    logging.info("Evaluating performance of CF recommender (all users, aisles)")
+    test_products = construct_test_product_dict(mode="test")
+    eval_dict = eval_recs(recs_dict=recs_dict, rating_df=test_ratings, test_products=test_products)
     
-    # # computing average hit-rate and ndcg
-    # avg_hr = np.mean([metric_dict["hit-rate"] for metric_dict in eval_dict.values()])
-    # avg_ndcg = np.mean([metric_dict[f"ndcg@{n_recs}"] for metric_dict in eval_dict.values()])
+    # computing average hit-rate and ndcg
+    avg_hr = np.mean([metric_dict["hit-rate"] for metric_dict in eval_dict.values()])
+    avg_ndcg = np.mean([metric_dict[f"ndcg@{n_recs}"] for metric_dict in eval_dict.values()])
 
-    # logging.info(f"The average hit-rate on the test set across all users is {avg_hr:.6f}")
-    # logging.info(f"The average ndcg@{n_recs} on the test set across all users is {avg_ndcg:.6f}")
+    logging.info(f"The average hit-rate on the test set across all users is {avg_hr:.6f}")
+    logging.info(f"The average ndcg@{n_recs} on the test set across all users is {avg_ndcg:.6f}")
 
-    # row = ["cf_all_users_aisles", n_recs, avg_hr, avg_ndcg]
-    # with open(RESULTS_PATH, mode="a", newline="") as file:
-    #     writer = csv.writer(file)
-    #     writer.writerow(row)
+    row = ["cf_all_users_aisles", n_recs, avg_hr, avg_ndcg]
+    with open(RESULTS_PATH, mode="a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
     
 
     ### CF - USER CLUSTERS ALL ITEMS ###
